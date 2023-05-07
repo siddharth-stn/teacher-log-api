3029195750a571035ce414658f58ab82bcf61954
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-05-07 18:44:35 +0530

    updated log model to return formatted date

--- models/log.js
+++ models/log.js
@@ -1,11 +1,18 @@
 const mongoose = require("mongoose");
 
+const { DateTime } = require("luxon");
+const { cachedDataVersionTag } = require("v8");
+
 const Schema = mongoose.Schema;
 
 const LogSchema = new Schema({
   date: { type: Date, default: Date.now, required: true },
   no_of_periods: { type: Number, required: true },
   data: { type: Object, required: true },
+});
+
+LogSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).setLocale("fr").toLocateString();
 });
 
 module.exports = mongoose.model("LogModel", LogSchema);
