54d038575c02bdc730256bdbe88f21616d1c7b6a
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-05-07 18:17:48 +0530

    added model for user

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: { type: String, required: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+  },
+  password: { type: String, required: true, trim: true },
+  isTeacher: {
+    type: Boolean,
+    enum: [true, false],
+    default: true,
+    required: true,
+  },
+});
+
+if (UserSchema.path("isTeacher").options.type === Boolean) {
+  UserSchema.add({
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: () => !this.isTeacher,
+    },
+  });
+}
+
+module.exports = mongoose.model("Usermodel", UserSchema);
