62ab8de169250c2fd4c49ec6d0420b7d784397e5
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-05-11 14:20:47 +0530

    refactored previously made routes and created some new log routes and created some new comment routes

--- routes/private.js
+++ routes/private.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 //* USER ROUTES
 /* Show users. */
-router.get("/users/list", function (req, res, next) {
+router.get("/users/list", function (req, res) {
   res.send("NOT IMPLEMENTED: SHOW USERS on GET");
 });
 
@@ -12,31 +12,65 @@ router.post("/user/create", function (req, res) {
 });
 
 /* Update User. */
-router.put("/user/update/:id", function (req, res, next) {
-  res.send(`NOT IMPLEMENTED: Update User on PUT, Id is: ${req.params.id}`);
+router.put("/user/update/:user_id", function (req, res) {
+  res.send(`NOT IMPLEMENTED: Update User on PUT, Id is: ${req.params.user_id}`);
 });
 
 /* Delete User. */
-router.delete("/user/delete/:id", function (req, res, next) {
-  res.send(`NOT IMPLEMENTED: Delete User on DELETE, Id is: ${req.params.id}`);
+router.delete("/user/delete/:user_id", function (req, res) {
+  res.send(
+    `NOT IMPLEMENTED: Delete User on DELETE, Id is: ${req.params.user_id}`
+  );
 });
 
 //* LOG ROUTES
-
 /* Show the list of logs for a particular user */
-router.post("/log/list/:user_id", function (req, res) {
+router.get("/log/list/:user_id", function (req, res) {
   res.send(
-    `NOT IMPLEMENTED: Show list of logs by date of entry, USER Id is: ${req.params.id}`
+    `NOT IMPLEMENTED: Show list of logs by date of entry, USER Id is: ${req.params.user_id}`
   );
 });
 
 /* Create Log */
 router.post("/log/create/:user_id", function (req, res) {
-  res.send(`NOT IMPLEMENTED: CREATE LOG on POST, USER Id is: ${req.params.id}`);
+  res.send(
+    `NOT IMPLEMENTED: CREATE LOG on POST, USER Id is: ${req.params.user_id}`
+  );
 });
 
+/* Delete Log */
 router.delete("log/delete/:log_id", function (req, res) {
-  res.send(`NOT IMPLEMENTED: DELETE LOG, Log Id is: ${req.params.id} `);
+  res.send(`NOT IMPLEMENTED: DELETE LOG, Log Id is: ${req.params.log_id}`);
+});
+
+/* Update Log */
+router.put("/log/update/:log_id", function (req, res) {
+  res.send(`NOT IMPLEMENTED: UPDATE LOG, Log Id is: ${req.params.log_id}`);
+});
+
+//* Comment Routes
+/* Show all comments for a log */
+router.get("/comment/list/:log_id", function (req, res) {
+  res.send(
+    `Show all comments for a particular log, log id is: ${req.params.log_id}`
+  );
+});
+
+/* Create a comment */
+router.post("/comment/create/:log_id", function (req, res) {
+  res.send(
+    `Create a comment for a particular log, log id is: ${req.params.log_id}`
+  );
+});
+
+/* Update a comment */
+router.put("/comment/update/:comment_id", function (req, res) {
+  res.send(`Update a comment, comment id is: ${req.params.comment_id}`);
+});
+
+/* Delete a comment */
+router.delete("/comment/delete/:comment_id", function (req, res) {
+  res.send(`Delete a comment, comment id is: ${req.params.comment_id}`);
 });
 
 module.exports = router;
