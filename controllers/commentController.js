17bc52a2cdf78ebf2d6df24536a384598767441e
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-05-27 16:58:47 +0530

    controller added for creating comment for a particular blog

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -15,3 +15,49 @@ exports.comments_list = async (req, res, next) => {
     return next(error);
   }
 };
+
+// Handle Comment Create on POST
+exports.comments_create = [
+  // log: { type: Schema.Types.ObjectId, ref: "Log", required: true },
+  // commentBody: { type: String, trim: true },
+  // isBySelf: {
+  //   type: Boolean,
+  //   enum: [true, false],
+  //   default: true,
+  //   required: true,
+  // },
+  // date: { type: Date, default: Date.now, required: true },
+
+  body("commentbody")
+    .trim()
+    .isLength({ min: 10 })
+    .withMessage("comment must be atleast 10 characters long")
+    .escape(),
+
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const comment = new Comment({
+      log: req.params.log_id,
+      commentBody: req.body.commentBody,
+      isBySelf: req.user.isAdmin === true ? false : true,
+    });
+
+    if (!errors.isEmpty()) {
+      // there are errors
+      // send back the json with sanitized values
+      res.json({
+        commentBody: req.body.no_of_periods,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      try {
+        await comment.save();
+        res.json("comment posted successfully");
+      } catch (error) {
+        return next(error);
+      }
+    }
+  },
+];
